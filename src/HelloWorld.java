b5a793b5bd3e915eeb02fce1f8617b8dfc08f225
S&A <S&A@10.31.9.214>
2013-04-25 09:59:29 +0200

    huhuu

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main (String [] args) {
 		
 		System.out.println("Hello World!");
-		System.out.println("Selam Millet");
+		System.out.println("huhuuuuu");
 		
 	}
 	
